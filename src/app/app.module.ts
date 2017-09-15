c3f103c82c773f4c5f78f2d914027014341558dd
juanb <juanbracamontes02@gmail.com>
2017-09-15 16:41:59 -0700

    Nuevo Componente Chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ChatComponent } from './chat/chat.component';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    NavbarComponent,
+    FooterComponent,
+    ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCtMYLASAR3ZTwz3voaooFVa8QuLg1aaps",
+    authDomain: "ejemplo-87d3b.firebaseapp.com",
+    databaseURL: "https://ejemplo-87d3b.firebaseio.com",
+    projectId: "ejemplo-87d3b",
+    storageBucket: "ejemplo-87d3b.appspot.com",
+    messagingSenderId: "687240019147"
+  }
 };
