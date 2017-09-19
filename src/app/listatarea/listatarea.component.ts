d0d208773d46ca4de23378c3e71f83a11d53c3c9
juanb <juanbracamontes02@gmail.com>
2017-09-19 14:51:54 -0700

    agregando mostrar tareas

--- src/app/listatarea/listatarea.component.ts
+++ src/app/listatarea/listatarea.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-listatarea',
   templateUrl: './listatarea.component.html'
 })
 export class ListatareaComponent implements OnInit {
+  tarea: FirebaseListObservable<any[]>;
+  nombretarea:string="";
 
-  constructor() { }
 
+  constructor(db: AngularFireDatabase) {
+      this.tarea= db.list('/tareas');
+  }
   ngOnInit() {
+  }
+  enviartarea(nombretarea:string){
+    this.tarea.push({
+      "nombretarea":nombretarea
+    })
+    this.nombretarea="";
   }
 
 }
